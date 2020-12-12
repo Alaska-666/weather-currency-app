1fd9cdf124ff8238c5bc8fa04cedf1f8304fb195
isaeva anna <pangolin-666@mail.ru>
2020-12-12 20:28:34 +0300

    fixed some sonar issues

--- src/main/java/ru/mipt/bit/homework/weathercurrencyapp/WeatherCurrencyAppApplication.java
+++ src/main/java/ru/mipt/bit/homework/weathercurrencyapp/WeatherCurrencyAppApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WeatherCurrencyAppApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(WeatherCurrencyAppApplication.class, args);
+        SpringApplication.run(WeatherCurrencyAppApplication.class);
     }
 
 }
