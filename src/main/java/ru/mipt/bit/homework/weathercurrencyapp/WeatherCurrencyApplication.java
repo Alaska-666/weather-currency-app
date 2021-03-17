60c0f5038c04b33aeac45c6ccbe752e93b263405
isaeva anna <pangolin-666@mail.ru>
2021-03-18 00:27:42 +0300

    added dockerfile

--- src/main/java/ru/mipt/bit/homework/weathercurrencyapp/WeatherCurrencyApplication.java
+++ src/main/java/ru/mipt/bit/homework/weathercurrencyapp/WeatherCurrencyApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WeatherCurrencyAppApplication {
+public class WeatherCurrencyApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(WeatherCurrencyAppApplication.class);
+        SpringApplication.run(WeatherCurrencyApplication.class);
     }
 
 }
